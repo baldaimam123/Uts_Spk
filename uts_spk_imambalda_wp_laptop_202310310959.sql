b9ae68387babba6a1ecdb932a335f83e012cb4cd
imam balda <118948272+baldaimam123@users.noreply.github.com>
2023-10-31 10:58:30 +0700

    Add files via upload

---
+++ uts_spk_imambalda_wp_laptop_202310310959.sql
@@ -0,0 +1,11 @@
+INSERT INTO "07TPLP015".uts_spk_imambalda_wp_laptop ("Nama Laptop",ram,"Memori Internal",processor,layar,"Harga (Rp)","Baterai (mAh)",alternatif,"Nilai Akhir","Rank") VALUES
+	 ('Acer Aspire 14','8 GB','256 GB','Intel Core i5','14 inci','12,000,000','10,000 mAh',2.6138048,0.102269925,5),
+	 ('ASUS VivoBook 15','16 GB','512 GB','AMD Ryzen 7','15,6 inci','18,000,000','8,000 mAh',2.649849,0.10368022,3),
+	 ('Dell XPS 13','12 GB','1 TB','Intel Core i7','13,3 inci','20,000,000','12,000 mAh',3.3156424,0.12973061,2),
+	 ('HP Stream 11','4 GB','128 GB','Intel Celeron','11,6 inci','6,000,000','6,000 mAh',1.4005417,0.05479877,10),
+	 ('Apple MacBook Air','16 GB','512 GB','Apple M1','13 inci','22,500,000','14,000 mAh',2.278411,0.08914702,7),
+	 ('Lenovo Ideapad 15','8 GB','256 GB','AMD Ryzen 5','15,6 inci','10,800,000','9,000 mAh',2.3084352,0.09032177,8),
+	 ('MSI GS75 Stealth','32 GB','2 TB','Intel Core i9','17,3 inci','30,000,000','8,000 mAh',2.6462631,0.103539914,4),
+	 ('Lenovo ThinkPad 14','8 GB','512 GB','AMD Ryzen 3','14 inci','8,400,000','7,000 mAh',2.0766113,0.08125124,9),
+	 ('HP Envy 15','16 GB','1 TB','Intel Core i7','15,6 inci','15,600,000','10,000 mAh',3.7915394,0.14835095,1),
+	 ('Dell Inspiron 13','8 GB','256 GB','Intel Core i5','13,3 inci','17,000,000','12,000 mAh',2.4768054,0.09690957,6);
